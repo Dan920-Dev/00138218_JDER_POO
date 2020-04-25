3e7f2cb34a98ee119ac8ec0a17d4e43243b387c0
Danii920 <escobare361@gmail.com>
2020-04-25 00:35:53 -0600

    Add: Labo 05 - Ejericio 02

---
+++ Labo 05/Ejercicio 02/Ejercicio 02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ejercicio_02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("¿La frase ingresada es Palindrome? " + funPalindrome());
+        }
+
+        static bool funPalindrome()
+        {
+            string sentenece = "";
+            Console.Write("Digite una frase, el programa determinara si es palindrome o no! ");
+            sentenece = Console.ReadLine();
+
+            sentenece = sentenece.ToLower();
+            sentenece = sentenece.Replace("\\s", "");
+            int size = sentenece.Length;
+
+            for (int i = 0; i < size; i++)
+            {
+                if (sentenece[i] == sentenece[size - 1 - i]) return true;
+            }
+
+            return false;
+        }
+    }
+}
