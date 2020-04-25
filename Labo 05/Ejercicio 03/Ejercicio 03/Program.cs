3366b1a8460c232286f300468c4e671743663f6a
Danii920 <escobare361@gmail.com>
2020-04-25 00:46:43 -0600

    Add: Labo 05 - Ejercicio 03

---
+++ Labo 05/Ejercicio 03/Ejercicio 03/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ejercicio_03
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Digite el valor de n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            if(n > 0){
+                funRecursive(n, 1, true);
+            }else{
+                Console.WriteLine("Numero ingresado es invalido!");
+            }
+        }
+        
+        static void funRecursive(int n, int baseNumber, bool valid){
+
+            if(valid) {
+                if(baseNumber < n){
+                    Console.Write(baseNumber + " ");
+                    baseNumber++;
+                    funRecursive(n, baseNumber, valid);
+                }else{
+
+                    if(baseNumber == n){
+                        valid = false;
+                        Console.Write(baseNumber + " ");
+                        baseNumber--;
+                        funRecursive(n, baseNumber, valid);
+                    }
+                }
+            }else {
+
+                if(baseNumber > 1){
+                    Console.Write(baseNumber + " ");
+                    baseNumber--;
+                    funRecursive(n, baseNumber, valid);
+                }else{
+
+                    Console.Write(baseNumber);
+                }
+            }
+
+        }
+    }
+}
