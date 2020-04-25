136d466b0c89c90e48fef0a92ada1b566b9bf393
Danii920 <escobare361@gmail.com>
2020-04-25 01:05:21 -0600

    Add: Labo 05 - Ejercicio 04

---
+++ Labo 05/Ejercicio 04/Ejercicio 04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio_04
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Digite una frase: ");
+            string sentence = Console.ReadLine();
+
+            sentence = sentence.Replace("a", "b");
+            
+            Console.Write(sentence);
+
+        }
+    }
+}
